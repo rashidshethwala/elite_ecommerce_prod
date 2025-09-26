c088a1681570403b87517d56fa2a2476477eacd2
Rashid Shethwala <abdulrashid.shethwala@thegatewaycorp.com>
2025-09-26 15:29:21 +0530

    updated allowed host

--- backend/ecommerce_backend/settings.py
+++ backend/ecommerce_backend/settings.py
@@ -136,6 +136,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://elite-ecommerce-prod-1.onrender.com",
 ]
 ALLOWED_HOSTS = [
     'elite-ecommerce-prod.onrender.com',
